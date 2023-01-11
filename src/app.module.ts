3a0441bfafa18272e23815e82a4a428161cec229
Jakub Padło <jakubpadlo11@gmail.com>
2023-01-11 10:18:23 +0100

    Add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+
+  constructor(configService: ConfigService) {
+    AppModule.port = configService.get('HTTP_PORT');
+  }
+}
